9d092cd9e21ebf998ce387b9d10378945602f5f1
Max-15 <m.canarius@gmail.com>
2022-05-11 23:58:34 -0400

    added shatter but CANT PRESS MENU BUTTON IN LATER LEVELS!
    not much

--- Assets/Scripts/SquareScript.cs
+++ Assets/Scripts/SquareScript.cs
@@ -29,7 +29,7 @@ public class SquareScript : MonoBehaviour
     }
     private void Update()
     {
-        if (myrow < WordleData.currentGuessNumber || data.eldroW)
+        if (myrow < WordleData.currentGuessNumber || WordleData.mode == WordleData.Mode.eldroW)
         {
             sr.material = toMaterial(WordleData.colours[myrow][mycolumn]);
         }
@@ -63,7 +63,7 @@ public class SquareScript : MonoBehaviour
     }
     private void OnMouseDown()
     {
-        if(data.eldroW && myrow <= WordleData.currentGuessNumber){
+        if(WordleData.mode == WordleData.Mode.eldroW && myrow <= WordleData.currentGuessNumber){
              WordleData.colours[myrow][mycolumn] = nextColour(WordleData.colours[myrow][mycolumn]);
         }
     }
